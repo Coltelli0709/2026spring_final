d2d56c8025f6f9a730476a2a37d09e10536e7dbc
Coltelli <jian190928@163.com>
2026-06-21 19:54:26 +0800

    [1014]

---
+++ 程设/W2 数制/1014/1014-1.cpp
@@ -0,0 +1,33 @@
+/*
+算了这种题就没必要浪费复习时间
+因为高位小一定小所以首位排1后边优先给高位排小的digit就完事了，再补一手unordered_map
+*/
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+//global variebles
+int T;
+
+
+int main(){
+    //speed up
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    //input
+    cin>>T;
+    for(int i = 0; i<T; i++){
+
+
+
+
+
+
+
+        //output
+        cout<<
+    }
+}
+*/
